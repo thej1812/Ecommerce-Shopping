31565bd010d357ba1597a887194f086a701b7145
thej1812 <thejashwini1800@gmail.com>
2026-01-16 22:43:50 +0530

    config

---
+++ backend/config/db.js
@@ -0,0 +1,9 @@
+// backend/config/db.js
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("MongoDB Connected");
+};
+
+export default connectDB;
