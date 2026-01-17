d79b0f65d627771d2e26703171db59e96ceb1c18
thej1812 <thejashwini1800@gmail.com>
2026-01-17 15:41:59 +0530

    product routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,6 +5,39 @@ import authMiddleware from "../middleware/authMiddleware.js";
 import adminMiddleware from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
+// ADMIN: UPDATE PRODUCT QUANTITY
+router.put(
+  "/:id/quantity",
+  authMiddleware,
+  adminMiddleware,
+  async (req, res) => {
+    try {
+      const { quantity } = req.body;
+
+      await Product.findByIdAndUpdate(req.params.id, {
+        quantity: Number(quantity)
+      });
+
+      res.json({ message: "Quantity updated" });
+    } catch (error) {
+      res.status(500).json({ message: "Update failed" });
+    }
+  }
+);
+// ADMIN: DELETE PRODUCT
+router.delete(
+  "/:id",
+  authMiddleware,
+  adminMiddleware,
+  async (req, res) => {
+    try {
+      await Product.findByIdAndDelete(req.params.id);
+      res.json({ message: "Product deleted" });
+    } catch (error) {
+      res.status(500).json({ message: "Delete failed" });
+    }
+  }
+);
 
 router.post(
   "/add",
@@ -15,10 +48,11 @@ router.post(
   async (req, res) => {
     try {
       const product = new Product({
-        name: req.body.name,
-        price: req.body.price,
-        image: req.file ? req.file.filename : ""
-      });
+  name: req.body.name,
+  price: Number(req.body.price),
+  quantity: Number(req.body.quantity),
+  image: req.file.filename
+});
 
       await product.save();
       res.json(product);
