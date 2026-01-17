ed35581daa32295e06e0ebc221ae332686257607
thej1812 <thejashwini1800@gmail.com>
2026-01-17 15:41:34 +0530

    poduct

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,14 +1,18 @@
-// backend/models/Product.js
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
   name: String,
   description: String,
   price: Number,
-  category: String,     // hairclip, earring
-  tag: String,          // women, men
-  type: String,         // normal, trending
-  image: String
+  category: String,
+  tag: String,
+  type: String, // normal / trending
+  image: String,
+
+  quantity: {
+    type: Number,
+    default: 0
+  }
 });
 
 export default mongoose.model("Product", productSchema);
