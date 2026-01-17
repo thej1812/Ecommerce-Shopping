68b5981437af99cb02371662b2d4258ab388ada4
thej1812 <thejashwini1800@gmail.com>
2026-01-17 15:43:31 +0530

    product

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -10,27 +10,51 @@ export default function Products() {
     axios.get("http://localhost:5000/api/products")
       .then(res => setProducts(res.data));
   }, []);
+  
+useEffect(() => {
+  const fetchProducts = () => {
+    fetch("http://localhost:5000/api/products")
+      .then(res => res.json())
+      .then(data => setProducts(data));
+  };
+
+  fetchProducts();
+  const interval = setInterval(fetchProducts, 3000);
+  return () => clearInterval(interval);
+}, []);
 
   return (
     <div className="grid grid-cols-3 gap-4 p-6">
       {products.map((product) => (
         <div key={product._id} className="border p-4">
-          <img
-            src={`http://localhost:5000/uploads/${product.image}`}
-            className="h-40 mx-auto"
-          />
+        <img className="h-40 mx-auto"
+  src={
+    product.image
+      ? `http://localhost:5000/uploads/${product.image}`
+      : ""
+  }
+/>
+
 
           <h2 className="font-bold">{product.name}</h2>
           <p>{product.description}</p>
           <p>₹{product.price}</p>
+         {product.quantity > 0 ? (
+  <button
+    onClick={() => addToCart(product)}
+    className="bg-black text-white px-4 py-2"
+  >
+    Add to Cart
+  </button>
+) : (
+  <button
+    disabled
+    className="bg-gray-400 text-white px-4 py-2 cursor-not-allowed"
+  >
+    Sold Out
+  </button>
+)}
 
-          {/* ✅ THIS IS THE IMPORTANT FIX */}
-          <button
-            onClick={() => addToCart(product)}
-            className="bg-black text-white px-4 py-2 mt-2"
-          >
-            Add to Cart
-          </button>
         </div>
       ))}
     </div>
