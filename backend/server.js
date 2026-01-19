970d4f3e361f1dbcae64b62c15323a8c5967b807
thej1812 <thejashwini1800@gmail.com>
2026-01-19 20:55:54 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
+import reviewRoutes from "./routes/reviewRoutes.js";
+
 dotenv.config();
 connectDB();
 
@@ -29,6 +31,7 @@ app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/categories", categoryRoutes);
+app.use("/api/reviews", reviewRoutes);
 /* ✅ IMAGE ACCESS */
 app.use("/uploads", express.static("uploads"));
 
