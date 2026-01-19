4e0e76eebf6e99f1105d5743ae1353a914c10964
thej1812 <thejashwini1800@gmail.com>
2026-01-19 20:56:01 +0530

    product

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,34 +1,38 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  price: Number,
-  category: String,
-  tag: String,
-  type: String, // normal / trending
-  image: String,
+const productSchema = new mongoose.Schema(
+  {
+    name: String,
 
-  quantity: {
-    type: Number,
-    default: 0
-  },
-  
-    images: {
-      type: [String], // array of image filenames
-      default: []
+    description: {
+      type: String,
+      default: "",
     },
 
-  category: {
-  type: mongoose.Schema.Types.ObjectId,
-  ref: "Category"
-},
-description: {
-  type: String,
-  default: ""
-},
+    price: Number,
 
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
 
-});
+    tag: String,
+
+    type: String, // normal / trending
+
+    image: String,
+
+    quantity: {
+      type: Number,
+      default: 0,
+    },
+
+    images: {
+      type: [String], // array of image filenames
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Product", productSchema);
