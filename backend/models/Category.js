2b35df392fccb680237a48d081602672c1618935
thej1812 <thejashwini1800@gmail.com>
2026-01-24 21:31:08 +0530

    category

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -1,11 +1,18 @@
 import mongoose from "mongoose";
 
-const categorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true
-  }
-});
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    image: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Category", categorySchema);
