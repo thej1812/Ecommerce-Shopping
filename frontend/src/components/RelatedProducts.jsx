474a05f757efe46759fb217cb518ff6de269e050
thej1812 <thejashwini1800@gmail.com>
2026-01-27 01:53:51 +0530

    realtedprodct

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -5,7 +5,7 @@ export default function RelatedProducts({ category, productId }) {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    if (!category) return; // 🔴 IMPORTANT GUARD
+    if (!category) return;
 
     fetch(
       `http://localhost:5000/api/products?category=${category}&exclude=${productId}`
@@ -24,25 +24,30 @@ export default function RelatedProducts({ category, productId }) {
   if (products.length === 0) return null;
 
   return (
-    <div className="mt-10">
-      <h2 className="text-xl font-bold mb-4">Related Products</h2>
+    <div className="pt-4 md:pt-6 pb-4 md:pb-6">
 
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-3 md:gap-4">
         {products.map(p => (
           <Link
             to={`/product/${p._id}`}
             key={p._id}
-            className="border p-2"
+            className=" p-3 md:p-6 block"
           >
             {p.images && p.images.length > 0 && (
               <img
                 src={`http://localhost:5000/uploads/${p.images[0]}`}
-                className="w-full h-40 object-cover"
+                className="h-44 w-44 md:h-64 md:w-full object-cover transition-transform duration-300 group-hover:scale-105"
                 alt={p.name}
               />
             )}
-            <p className="mt-2 font-semibold">{p.name}</p>
-            <p>₹{p.price}</p>
+
+            <p className="text-sm mt-4 text-gray-800 font-[Mulish]">
+              {p.name}
+            </p>
+
+            <p className="text-sm text-gray-500 mt-1 font-[Mulish]">
+              ₹{p.price}
+            </p>
           </Link>
         ))}
       </div>
