a297d9338559eb449b1c6f5842e720728f15553b
bhanu-3333 <bhanuu339@gmail.com>
2026-03-07 19:11:15 +0530

    Add Vercel URL to CORS

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:4173",
-  "https://ecommerce-shopping-theta.vercel.app"
+  "https://ecommerce-shopping-theta.vercel.app",
+  "https://ecommerce-shopping-tdvj.vercel.app"
 ];
 
 app.use(
