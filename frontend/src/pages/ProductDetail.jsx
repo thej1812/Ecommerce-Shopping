c242b941d7f03a92eeb8be5ae106f9a418a611e8
thej1812 <thejashwini1800@gmail.com>
2026-03-11 20:52:09 +0530

    preact: update UI elements for better readability and user experience

--- frontend/src/pages/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail.jsx
@@ -134,14 +134,24 @@ export default function ProductDetail() {
         <img src="/logo.png" alt="Brand Logo" className="h-10 md:h-14" />
 
         <p className="text-xs text-gray-500 font-[Mulish] text-center">
-          © Copyrights-2025
+          © Copyrights-2025 | Crafted by <a href="https://www.instagram.com/laaz_creative?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" className="underline">Laaz Creative</a>
         </p>
 
-        <div className="flex gap-5">
-          <img src="/youtube.png" alt="YouTube" className="h-5 w-6" />
-          <img src="/whatsapp.png" alt="Whatsapp" className="h-5 w-5" />
-          <img src="/instagram.png" alt="Instagram" className="h-5 w-5" />
-        </div>
+         <div className="flex gap-5">
+
+  <a href="https://youtube.com/@radhi_vedha?si=i9Bew2CEhv519gBH" target="_blank" rel="noopener noreferrer">
+    <img src="/youtube.png" alt="YouTube" className="h-5 w-6 cursor-pointer" />
+  </a>
+
+  <a href="https://wa.me/919962658353" target="_blank" rel="noopener noreferrer">
+    <img src="/whatsapp.png" alt="Whatsapp" className="h-5 w-5 cursor-pointer" />
+  </a>
+
+  <a href="https://www.instagram.com/radhi_vedha" target="_blank" rel="noopener noreferrer">
+    <img src="/instagram.png" alt="Instagram" className="h-5 w-5 cursor-pointer" />
+  </a>
+
+</div>
       </div>
 
     </div>
